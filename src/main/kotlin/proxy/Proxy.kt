b85a2a50c22a17d33cd172532909498a1460a239
Alexey Soshin <alexey.soshin@gmail.com>
2020-02-09 18:00:48 +0200

    Implement proxying any type of HttpMethod

--- src/main/kotlin/proxy/Proxy.kt
+++ src/main/kotlin/proxy/Proxy.kt
@@ -5,21 +5,25 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpStatement
 import io.ktor.client.statement.readBytes
-import io.ktor.http.HttpMethod
+import io.ktor.request.httpMethod
 import io.ktor.request.receiveChannel
 import io.ktor.request.uri
 import io.ktor.response.respond
+import io.ktor.util.filter
 import io.ktor.util.toByteArray
 
 
 private val client = HttpClient()
 
+private val readOnlyHeaders = listOf("Content-Length", "Content-Type")
+
 suspend fun ApplicationCall.proxy(target: String) {
     val call = this
     val response = client.request<HttpStatement>("$target/${call.request.uri}") {
-        this.method = HttpMethod.Get
+        this.method = call.request.httpMethod
         this.body = call.receiveChannel().toByteArray()
-        call.request.headers.forEach { key, values ->
+        call.request.headers.filter { key, _ -> !readOnlyHeaders.contains(key) }
+                .forEach { key, values ->
             this.headers[key] = values.first()
         }
     }.execute()
--- src/test/kotlin/proxy/WebServer.kt
+++ src/test/kotlin/proxy/WebServer.kt
@@ -1,42 +1,68 @@
 package proxy
+
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.client.HttpClient
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpStatement
 import io.ktor.client.statement.readText
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receiveChannel
+import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProxyTest {
+
+    val proxyServer = embeddedServer(Netty, 28080, module = Application::proxyModule)
+    val server = embeddedServer(Netty, 28081, module = Application::mainModule)
+
+    @BeforeAll
+    fun start() {
+        proxyServer.start()
+        server.start()
+    }
+
+    @AfterAll
+    fun stop() {
+        proxyServer.stop(10, 100)
+        server.stop(10, 100)
+    }
 
     @Test
     fun `proxies get calls`() {
         runBlocking {
-            val proxyServer = embeddedServer(Netty, 28080, module = Application::proxyModule)
-            val server = embeddedServer(Netty, 28081, module = Application::mainModule)
-            try {
-                proxyServer.start()
-                server.start()
+            val client = HttpClient()
 
-                val client = HttpClient()
+            val response = client.request<HttpStatement>("http://localhost:28080/articles/123/edit").execute()
 
-                val response = client.request<HttpStatement>("http://localhost:28080/articles/123/edit").execute()
+            assertEquals("123", response.readText())
+        }
+    }
 
-                assertEquals("123", response.readText())
-            }
-            finally {
-                proxyServer.stop(10, 100)
-                server.stop(10, 100)
-            }
+    @Test
+    fun `proxies post calls`() {
+        runBlocking {
+            val client = HttpClient()
+
+            val response = client.request<HttpStatement>("http://localhost:28080/articles/123") {
+                this.method = HttpMethod.Post
+                this.body = "ABC"
+            }.execute()
+
+            assertEquals("ABC", response.readText())
         }
     }
 }
@@ -46,6 +72,9 @@ fun Application.mainModule() {
         get("/articles/{id}/edit") {
             call.respond(HttpStatusCode.Accepted, call.parameters["id"] ?: "")
         }
+        post("/articles/{id}") {
+            call.respond(call.receiveText())
+        }
     }
 }
 
@@ -54,6 +83,9 @@ fun Application.proxyModule() {
     val target = "http://localhost:28081"
     routing {
         get("/articles/{id}/edit") {
+            call.proxy(target)
+        }
+        post("/articles/{id}") {
             call.proxy(target)
         }
     }
